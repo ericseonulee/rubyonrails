ac97eea70366c6df4ddb129596e36e35de2fad99
Eric Lee <szl0094@auburn.edu>
2022-10-05 18:53:02 -0500

    Generated a Model, database migration, showing the list of article.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
